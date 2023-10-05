4f7de5e6e4a08dd41560db993cbef37d7b978764
baonguyen2310 <lanchinhaicon@gmail.com>
2023-10-05 21:37:59 +0700

    add: slug in product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { model, Schema, Types } = require('mongoose')
+const slugify = require('slugify')
 
 const DOCUMENT_NAME = 'Product'
 const COLLECTION_NAME = 'Products'
@@ -9,14 +10,31 @@ const productSchema = new Schema({
     product_name: { type: String, required: true },
     product_thumb: { type: String, require: true },
     product_description: { type: String },
+    product_slug: { type: String },
     product_price: { type: Number, require: true },
     product_quantity: { type: Number, require: true },
     product_type: { type: String, require: true, enum: ['Clothing', 'Electronic', 'Furniture'] },
     product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
-    product_attributes: { type: Schema.Types.Mixed, require: true }
+    product_attributes: { type: Schema.Types.Mixed, require: true },
+    product_ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be under 5.0'],
+        set: (val) => Math.round(val * 10) / 10
+    },
+    product_variations: { type: Array, default: [] }, // các biến thể
+    isDraft: { type: Boolean, default: true, index: true, select: false },
+    isPublished: { type: Boolean, default: false, index: true, select: false }
 }, {
     collection: COLLECTION_NAME,
     timestamps: true
+})
+
+// Document middleware: run before .save() or .create() ...
+productSchema.pre('save', function(next){
+    this.product_slug = slugify(this.product_name, {lower: true})
+    next()
 })
 
 const clothingSchema = new Schema({
